114d71dcfb8275583cc0cbcecbc9c2f2c3a6868f
Alcides Dias <cidesjour13@gmail.com>
2019-05-05 16:53:30 -0300

    Adicionado url para ver detalhes do poema

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -6,3 +6,7 @@ def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     last = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date').reverse()[0]
     return render(request, 'blog/index.html', {'posts': posts, 'last': last})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
