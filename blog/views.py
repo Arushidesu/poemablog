665464fbe283c5b27e2d0ca1c66f39a383fc5309
Alcides Dias <cidesjour13@gmail.com>
2019-05-05 16:05:15 -0300

    Template dinâmico

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def index(request):
-    return render(request, 'blog/index.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    last = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date').reverse()[0]
+    return render(request, 'blog/index.html', {'posts': posts, 'last': last})
